05cf2aebd9f842758f53074a646ad66789dc177a
백지원 <qorjiwon@baegjiwon-ui-MacBookAir.local>
2025-10-18 11:57:24 +0900

    chore: remove unused codes and files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "quill/dist/quill.snow.css";
 import Providers from "./providers";
-import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
